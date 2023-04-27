1c973be37b235ae1da1e14fb7e274476749bd1d2
nishantuchil <nishantuchil@gmail.com>
2023-04-27 21:33:17 +0530

    Initial checkin for linear regression model

---
+++ SparkModule3/src/main/java/com/virtualpairprogrammers/GymCompetitors.java
@@ -0,0 +1,62 @@
+/** */
+package com.virtualpairprogrammers;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+/** @author uchil */
+public class GymCompetitors {
+
+  /** @param args */
+  public static void main(String[] args) {
+
+    LoggerContext context = (LoggerContext) LogManager.getContext(false);
+    Configuration logConfiguration = context.getConfiguration();
+    LoggerConfig loggerConfig = logConfiguration.getLoggerConfig("org.apache");
+    loggerConfig.setLevel(Level.WARN);
+    context.updateLoggers();
+
+    SparkSession spark =
+        SparkSession.builder()
+            .appName("GymCompetitors")
+            .config("spark.sql.warehouse.dir", "file:///c:/tmp/")
+            .master("local[*]")
+            .getOrCreate();
+
+    Dataset<Row> csvData =
+        spark
+            .read()
+            .option("header", true)
+            .option("inferSchema", true)
+            .csv("src/main/resources/GymCompetition.csv");
+
+    csvData.schema();
+
+    VectorAssembler vectorAssembler = new VectorAssembler();
+    vectorAssembler.setInputCols(new String[] {"Age", "Height", "Weight"});
+    vectorAssembler.setOutputCol("features");
+    Dataset<Row> csvDataWithFeatures = vectorAssembler.transform(csvData);
+    Dataset<Row> modelInputData =
+        csvDataWithFeatures.select("NoOfReps", "features").withColumnRenamed("NoOfReps", "label");
+    modelInputData.show();
+
+    LinearRegression linearRegression = new LinearRegression();
+    LinearRegressionModel model = linearRegression.fit(modelInputData);
+    System.out.println(
+        "The model has intercept "
+            + model.intercept()
+            + " and coeffcients "
+            + model.coefficients());
+
+    model.transform(modelInputData).show();
+  }
+}
